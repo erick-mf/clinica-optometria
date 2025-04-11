74a1027b3a00b2423e1a4c4eb80dc72db71056e2
Daniel Trivino <danieltrivinomarin@gmail.com>
2025-04-11 10:56:15 +0200

    arreglo del la altura del mensaje y usuario del header

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,8 +3,8 @@
 @php
     $classes =
         $active ?? false
-            ? 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 border-b-2 border-white  hover:border-gray-200 text-white hover:text-gray-200 focus:outline-none transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-white hover:text-gray-200 focus:outline-none focus:text-gray-50 transition duration-150 ease-in-out';
+            ? 'inline-flex items-center px-1 text-sm font-medium leading-5 border-b-2 border-white  hover:border-gray-200 text-white hover:text-gray-200 focus:outline-none transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 text-sm font-medium leading-5 text-white hover:text-gray-200 focus:outline-none focus:text-gray-50 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
